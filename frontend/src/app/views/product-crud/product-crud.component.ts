e4a1d2b753a93d2502b8f1831a23f3dd1faebf46
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2022-05-30 21:15:22 -0300

    first commit

---
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-crud',
+  templateUrl: './product-crud.component.html',
+  styleUrls: ['./product-crud.component.css']
+})
+export class ProductCrudComponent implements OnInit {
+
+  propLegal = "qualquer";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  fazerAlgo(): void {
+    console.log('fazendo algo');
+  }
+
+}
